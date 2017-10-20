dc5f031160ca874df8e067bbce84c0f8efef3fd5
Poels Thibault <thibault.poels@immalle.eu>
2017-10-20 10:55:56 +0200

    Add project files.

---
+++ ConsoleLoopTry/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleLoopTry
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
