0c962d6ca042eed529656aac4f9278df11f85729
ThibaultP-immalle <31651240+ThibaultP-immalle@users.noreply.github.com>
2017-10-27 10:53:36 +0200

    Change 1
    change 1

--- ConsoleLoopTry/Program.cs
+++ ConsoleLoopTry/Program.cs
@@ -1,12 +1,21 @@
 ﻿using System;
+using System.Threading;
 
 namespace ConsoleLoopTry
 {
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        {         
+
+            int x = 1000000000;
+            while (x > 3)
+            {
+                x--;
+                Thread.Sleep(20);
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
+                Console.WriteLine(x + "Q"+ x + "V" + x + "D" + x + "H" + x + "K" + x + "M" + x + "N" + x + "C" + x + "A" + x + "Y" + x + "I" + x );
+            }
         }
     }
 }
